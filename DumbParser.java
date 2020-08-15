e7f795b9162f2a58d7d35613bc17742f2ab048aa
Pablo Martínez <pablomartinez1017@gmail.com>
2020-08-15 11:49:31 +0200

    Estructura de YAML y pequeños cambios. Detectado por SemMerge pero no detecta los archivos

--- DumbParser.java
+++ DumbParser.java
@@ -3,85 +3,116 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.io.File;
 import java.util.*;
 
-public class DumbParser{
-    public static void main (String args[]){
+public class DumbParser {
+    public static void main(String args[]) {
         try (PrintWriter out = new PrintWriter(args[1])) {
             out.println("READY");
-            String flagfile= args[1];
-            out.close();
-            //Files.delete(Path.of(flagfile, ""));
-        //}
-
-        /*try{
-            File flag = new File(args[1]);
-            FileWriter fw = new FileWriter(flag);
-            fw.write("READY");
-            fw.close();
-            flag.delete();*/
-        }catch(IOException ex){
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
         Scanner scanner = new Scanner(System.in);
 
         String firstFile = scanner.nextLine();
-        //String firstEncoding = scanner.nextLine();
+        String firstEncoding = scanner.nextLine();
         String firstFileOutput = scanner.nextLine();
-        ParseFile(firstFile,firstFileOutput);
+        ParseFile(firstFile, firstFileOutput);
         System.out.println("OK");
 
         String secondFile = scanner.nextLine();
-        //String secondEncoding = scanner.nextLine();
+        String secondEncoding = scanner.nextLine();
         String secondFileOutput = scanner.nextLine();
-        ParseFile(secondFile,secondFileOutput);
+        ParseFile(secondFile, secondFileOutput);
         System.out.println("OK");
 
         String end = scanner.nextLine();
         scanner.close();
-        if(end.equals("end")){
+        if (end.equals("end")) {
             return;
         }
 
     }
 
-    private static void ParseFile(String input, String output){
-        try{
+    private static void ParseFile(String input, String output) {
+        try {
             String text = new String(Files.readAllBytes(Paths.get(input)), StandardCharsets.UTF_8);
-            if(text.indexOf("yyy")<0){
-                ParseSource(input,output);
+            if (text.indexOf("mTimeout") < 0) {
+                ParseSource(input, output);
                 return;
             }
-            ParseDestination(input,output);
-        }catch(IOException ex){
-            ex.printStackTrace();
-        }
-        //ParseDestination(input,output);
-    }
-
-    private static void ParseSource(String input, String output){
-        try{
-            File outF = new File(output + ".txt");
-            FileWriter fw = new FileWriter(outF);
-            fw.write("Source 1\n");
-            fw.write("Source 2\n");
-            fw.close();
-        }catch(IOException ex){
+            ParseDestination(input, output);
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 
-    private static void ParseDestination(String input, String output){
-        try{
-            File outF = new File(output + ".txt");
+    private static void ParseSource(String input, String output) {
+        try {
+            File outF = new File(output + ".yaml");
             FileWriter fw = new FileWriter(outF);
-            fw.write("Destination 1\n");
-            fw.write("Destination 2\n");
+            fw.write("---\n");
+            fw.write("type: file\n");
+            fw.write("name: {0} " + input+"\n");
+            fw.write("locationSpan : {start: [1,0], end: [12,1]}\n");
+            fw.write("footerSpan : [0,-1]\n");
+            fw.write("parsingErrorsDetected : false\n");
+            fw.write("children:\n");
+
+            fw.write("  - type : class\n");
+            fw.write("    name : Socket\n");
+            fw.write("    locationSpan : {start: [1,0], end: [12,1]}\n");
+            fw.write("    headerSpan : [0, 16]\n");
+            fw.write("    footerSpan : [186, 186]\n");
+            fw.write("    children :\n");
+
+            fw.write("    - type : method\n");
+            fw.write("      name : Connect\n");
+            fw.write("      locationSpan : {start: [3, 0], end: [7,2]}\n");
+            fw.write("      span : [17, 109]\n");
+
+            fw.write("    - type : method\n");
+            fw.write("      name : Disconnect\n");
+            fw.write("      locationSpan : {start: [8,0], end: [11,6]}\n");
+            fw.write("      span : [110, 185]\n");
             fw.close();
-        }catch(IOException ex){
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static void ParseDestination(String input, String output) {
+        try {
+            File outF = new File(output + ".yaml");
+            FileWriter fw = new FileWriter(outF);
+            fw.write("---\n");
+            fw.write("type: file\n");
+            fw.write("name: {0} "+ input+"\n");
+            fw.write("locationSpan : {start: [1,0], end: [14,1]}\n");
+            fw.write("footerSpan : [0,-1]\n");
+            fw.write("parsingErrorsDetected : false\n");
+            fw.write("children:\n");
+
+            fw.write("  - type : class\n");
+            fw.write("    name : Socket\n");
+            fw.write("    locationSpan : {start: [1,0], end: [14,1]}\n");
+            fw.write("    headerSpan : [0, 20]\n");
+            fw.write("    footerSpan : [200, 200]\n");
+            fw.write("    children :\n");
+
+            fw.write("    - type : method\n");
+            fw.write("      name : Disconnect\n");
+            fw.write("      locationSpan : {start: [5, 0], end: [9,2]}\n");
+            fw.write("      span : [21, 98]\n");
+
+            fw.write("    - type : method\n");
+            fw.write("      name : Connect\n");
+            fw.write("      locationSpan : {start: [10,0], end: [13,6]}\n");
+            fw.write("      span : [99, 199]\n");
+            fw.close();
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
